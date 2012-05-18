aa0821744f1f9eee9741411c85004fa015164675
Brent W. Barker <barker@nscl.msu.edu>
2012-05-17 20:24:46 -0400

    Added parameter 'maxdouble' to prec_def.f90, representing the maximum storable positive number in a double precision floating-point number.

--- src/prec_def.f90
+++ src/prec_def.f90
@@ -3,6 +3,10 @@ MODULE prec_def
  use ISO_FORTRAN_ENV
   IMPLICIT NONE
 
+ !> Maximum positive value of a double precision number. From
+ !! https://en.wikipedia.org/w/index.php?title=Double-precision_floating-point_format&oldid=489929288
+ real*8, parameter :: maxdouble=(1.d0+(1.d0-2.d0**(-52)))*2.d0**1023
+
   !> KIND of numbers. REAL64 is the KIND number of a 64-bit real number
   !! in this architecture (provided by ISO_FORTRAN_ENV)
   INTEGER,PARAMETER :: long=REAL64
@@ -11,6 +15,8 @@ MODULE prec_def
   !! determining whether two floating-point numbers are equal. For example,
   !! if(abs(a-b)<epzero) write(*,*)'a and be are equal'
   real(Long) :: epzero = (2._Long)**(-52)
+
+  real (Long), parameter :: maxlong=maxdouble
 
   integer,parameter :: stderr=102  !file unit of standard error
   
