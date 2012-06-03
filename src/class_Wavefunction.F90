7a179cc2fdeb2d50a1e21925158ca3f401a0caa2
Brent W. Barker <b.w.barker@smokejive.net>
2012-06-03 14:21:38 -0400

    Added src/class_Wavefunction.F90, a module.

---
+++ src/class_Wavefunction.F90
@@ -0,0 +1,31 @@
+!> parent class of wavefunctions, so they can be put in an array together.
+module class_Wavefunction
+ use prec_def
+ implicit none
+
+ private
+
+ public :: Wavefunction
+
+ type, abstract :: Wavefunction 
+  contains
+   procedure(getWavefn), deferred :: getWavefn
+
+ end type Wavefunction
+
+ interface
+
+  real(Long) function getWavefn(this,xx,tt)
+   use prec_def
+   import Wavefunction
+ 
+   class(Wavefunction),  intent(in) :: this
+   real(Long),           intent(in) :: xx
+   real(Long), optional, intent(in) :: tt
+
+  end function getWavefn
+
+ end interface
+
+end module class_Wavefunction
+
