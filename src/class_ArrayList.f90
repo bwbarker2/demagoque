ea287d8c91cb9b32408a833909f0d6ee6585e802
Brent W. Barker <b.w.barker@smokejive.net>
2013-02-07 21:14:33 -0500

    Added integer type of ArrayList. dArrayList now uses KIND=Long and uses prec_def. It is now F2008 compliant.
    src/class_ArrayList.f90: Added bexception and prec_def modules. Made everything private except types and their constructors.
    src/class_ArrayList.f90 (iArrayList, new_dArrayList, iArrayList_add, iArrayList_ensureCapacity, iArrayList_get, iArrayList_set, iArrayList_size, iReallocate): Added.

--- src/class_ArrayList.f90
+++ src/class_ArrayList.f90
@@ -23,9 +23,13 @@ module class_ArrayList
 !            National Superconducting Cyclotron Laboratory
 !            Michigan State University
 !            1 Cyclotron, East Lansing, MI 48824-1321
+use bexception
+use prec_def
 implicit none
 
- private :: INITIAL_LENGTH, reallocate
+ private
+
+ public :: dArrayList, iArrayList, new_dArrayList, new_iArrayList
 
  integer, parameter :: INITIAL_LENGTH=10
 
@@ -33,7 +37,7 @@ implicit none
   private
   integer :: capacity
   integer :: sizeOfIt  !"OfIt" is so it is different from size below
-  real*8, dimension(:), allocatable  :: values
+  real(Long), dimension(:), allocatable  :: values
  contains
   procedure :: add => dArrayList_add
   procedure :: ensureCapacity => dArrayList_ensureCapacity
@@ -41,15 +45,31 @@ implicit none
   procedure :: set => dArrayList_set
   procedure :: size => dArrayList_size
  end type dArrayList
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ type iArrayList
+  private
+  integer :: capacity
+  integer :: sizeOfIt  !"OfIt" is so it is different from size below
+  integer, dimension(:), allocatable  :: values
+ contains
+  procedure :: add => iArrayList_add
+  procedure :: ensureCapacity => iArrayList_ensureCapacity
+  procedure :: get => iArrayList_get
+  procedure :: set => iArrayList_set
+  procedure :: size => iArrayList_size
+ end type iArrayList
  
 contains
 
- function make_dArrayList(initLength) result (self)
+ !> constructs new dArrayList with optional initial length
+ function new_dArrayList(initLength) result (self)
   implicit none
 
-  integer, optional, intent(in) :: initLength
+  integer, optional, intent(in) :: initLength !< initial length of list
   type (dArrayList) :: self
-  real*8, dimension(:), allocatable :: list_construct
+  real(Long), dimension(:), allocatable :: list_construct
 
   if(present(initLength)) then
    allocate(list_construct(initLength))
@@ -59,18 +79,37 @@ contains
    self = dArrayList(INITIAL_LENGTH,0,list_construct)
   endif
 
- end function make_dArrayList
+ end function new_dArrayList
+
+ !> constructs new iArrayList with optional initial length
+ function new_iArrayList(initLength) result (self)
+  implicit none
+
+  integer, optional, intent(in) :: initLength !< initial length of list
+  type (iArrayList) :: self
+  integer, dimension(:), allocatable :: list_construct
+
+  if(present(initLength)) then
+   allocate(list_construct(initLength))
+   self = iArrayList(initLength,0,list_construct)
+  else
+   allocate(list_construct(INITIAL_LENGTH))
+   self = iArrayList(INITIAL_LENGTH,0,list_construct)
+  endif
+
+ end function new_iArrayList
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+ !> Appends item to end of list
  subroutine dArrayList_add(self, newValue)
   implicit none
 
   class (dArrayList), intent(inout) :: self
-  real*8,            intent(in)    :: newValue
+  real(Long),            intent(in)    :: newValue ! value to add to list
 
   if(self%size()==self%capacity) then
-   call reallocate(self,self%capacity*2)
+   call dReallocate(self,self%capacity*2)
   endif
 
   self%values(self%size()+1)=newValue
@@ -80,26 +119,63 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+ !> Appends item to end of list
+ subroutine iArrayList_add(self, newValue)
+  implicit none
+
+  class (iArrayList), intent(inout) :: self
+  integer,            intent(in)    :: newValue ! value to add to list
+
+  if(self%size()==self%capacity) then
+   call iReallocate(self,self%capacity*2)
+  endif
+
+  self%values(self%size()+1)=newValue
+  self%sizeOfIt=self%size()+1
+
+ end subroutine iArrayList_add
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Ensures that list can store 'newCapacity' items. Does not reduce
+ !! capacity.
  subroutine dArrayList_ensureCapacity(self, newCapacity)
   implicit none
 
   class (dArrayList), intent(inout) :: self
-  integer,           intent(in)    :: newCapacity
+  integer,           intent(in)    :: newCapacity !< capacity to ensure
 
   if(self%capacity<newCapacity) then
-   call reallocate(self, newCapacity)
+   call dReallocate(self, newCapacity)
   endif
 
  end subroutine dArrayList_ensureCapacity
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+ !> Ensures that list can store 'newCapacity' items. Does not reduce
+ !! capacity.
+ subroutine iArrayList_ensureCapacity(self, newCapacity)
+  implicit none
+
+  class (iArrayList), intent(inout) :: self
+  integer,           intent(in)    :: newCapacity !< capacity to ensure
+
+  if(self%capacity<newCapacity) then
+   call iReallocate(self, newCapacity)
+  endif
+
+ end subroutine iArrayList_ensureCapacity
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Returns value at a given index.
  function dArrayList_get(self, indix) result(val)
   implicit none
 
   class(dArrayList), intent(in) :: self
-  integer,         intent(in) :: indix
-  real*8                      :: val
+  integer,         intent(in) :: indix  !< index of value to return
+  real(Long)                      :: val
 
   if(indix<=self%size()) then
    val=self%values(indix)
@@ -114,12 +190,34 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+ !> Returns value at a given index.
+ function iArrayList_get(self, indix) result(val)
+  implicit none
+
+  class(iArrayList), intent(in) :: self
+  integer,           intent(in) :: indix  !< index of value to return
+  integer                      :: val
+
+  if(indix<=self%size()) then
+   val=self%values(indix)
+  else
+   write(ERROR_UNIT,*)'iArrayList_get: index,size=',indix,self%size()
+   call throwException('iArrayList_get: ArrayOutOfBoundsError, index,size=' &
+                       ,BEXCEPTION_FATAL)
+  endif
+
+ end function iArrayList_get
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Sets a element to a value. That element must already have a value
+ !! in it (size of the list must be >= index requested)
  subroutine dArrayList_set(self, indix, val)
   implicit none
 
-  class(dArrayList), intent(inout) :: self
-  integer,          intent(in)    :: indix
-  real*8,           intent(in)    :: val
+  class(dArrayList), intent(inout) :: self 
+  integer,          intent(in)    :: indix !< index of element to set
+  real(Long),           intent(in)    :: val   !< new value for the element
 
   if(indix<=self%size()) then
    self%values(indix)=val
@@ -133,6 +231,27 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+ !> Sets a element to a value. That element must already have a value
+ !! in it (size of the list must be >= index requested)
+ subroutine iArrayList_set(self, indix, val)
+  implicit none
+
+  class(iArrayList), intent(inout) :: self 
+  integer,           intent(in)    :: indix !< index of element to set
+  integer,           intent(in)    :: val   !< new value for the element
+
+  if(indix<=self%size()) then
+   self%values(indix)=val
+  else
+   write(ERROR_UNIT,*)'iArrayList_set: indix,size=',indix,self%size()
+   call throwException('iArrayList_set: ArrayOutOfBoundsError',BEXCEPTION_FATAL)
+  endif
+
+ end subroutine iArrayList_set
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Returns logical size of list.
  integer function dArrayList_size(self)
   implicit none
 
@@ -144,13 +263,27 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
- subroutine reallocate(self, newCapacity)
+ !> Returns logical size of list.
+ integer function iArrayList_size(self)
+  implicit none
+
+  class(iArrayList), intent(in) :: self
+
+  iArrayList_size=self%sizeOfIt
+
+ end function iArrayList_size
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Changes capacity of storage array. If newCapacity < current capacity,
+ !! then it truncates, potentially losing information and reducing size.
+ subroutine dReallocate(self, newCapacity)
   implicit none
 
   class (dArrayList), intent(inout) :: self
   integer,           intent(in)    :: newCapacity
 
-  real*8, dimension(self%capacity) :: newList
+  real(Long), dimension(self%capacity) :: newList
 
   if(newCapacity==self%capacity) return
 
@@ -159,12 +292,39 @@ contains
   allocate(self%values(newCapacity))
   if(newCapacity<self%capacity) then
    self%values=newList(1:newCapacity)
+   if(newCapacity<self%sizeOfIt) self%sizeOfIt=newCapacity
   else
    self%values(1:self%capacity)=newList
   endif
   self%capacity=newCapacity
 
- end subroutine reallocate
+ end subroutine dReallocate
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ !> Changes capacity of storage array. If newCapacity < current capacity,
+ !! then it truncates, potentially losing information and reducing size.
+ subroutine iReallocate(self, newCapacity)
+  implicit none
+
+  class (iArrayList), intent(inout) :: self
+  integer,           intent(in)    :: newCapacity
+
+  integer, dimension(self%capacity) :: newList
+
+  if(newCapacity==self%capacity) return
+
+  newList=self%values
+  deallocate(self%values)
+  allocate(self%values(newCapacity))
+  if(newCapacity<self%capacity) then
+   self%values=newList(1:newCapacity)
+   if(newCapacity<self%sizeOfIt) self%sizeOfIt=newCapacity
+  else
+   self%values(1:self%capacity)=newList
+  endif
+  self%capacity=newCapacity
+
+ end subroutine iReallocate
 
 end module class_ArrayList
